8954f0ba17bdc32e8b4ba2282aaeb92281b54e41
harrah20 <harrah20@up.edu>
2020-02-04 13:48:51 -0800

    cheesecake files added to webpage

---
+++ hw3/public/javascripts/cheesecake.js
@@ -0,0 +1,10 @@
+$('textarea').keyup(function() {
+					if ($(this).val().toLowerCase().indexOf('vegan') != -1) {
+						$("body").append("<h3>WARNING: Cheesecake contains dairy</h3>")
+					}
+					else(){
+						$("body").remove("div")
+						$("body").remove("p")
+						$("body").append("<h3>Thank you! Your order has been placed</h3>")
+					}
+				});
